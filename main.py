2e11cca9957c28128e46a0c0ee4d8c9e11b050c2
Zhang-Yuxuan <Zhang_YuxuanCN@outlook.com>
2024-03-08 12:05:51 +0800

    add epic1 task9 to task11，pass the test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import
 from pyspark.sql import SparkSession
+from epics import  epic1
 
 # Driver
 spark = SparkSession \
@@ -16,3 +17,19 @@ checkin_path = 'dataset/yelp_academic_dataset_checkin.json'
 review_path = 'dataset/yelp_academic_dataset_review.json'
 tip_path = 'dataset/yelp_academic_dataset_tip.json'
 user_path = 'dataset/yelp_academic_dataset_user.json'
+
+# epic1 测试代码
+# b_df = spark.read.json(business_path)
+# r_df = spark.read.json(review_path)
+
+# epic1.epic1_task1(b_df).show()
+# epic1.epic1_task2(b_df).show()
+# epic1.epic1_task3(b_df).show()
+# epic1.epic1_task4(b_df).show()
+# epic1.epic1_task5(b_df).show()
+# print(epic1.epic1_task6(b_df))
+# epic1.epic1_task7(b_df).show()
+# epic1.epic1_task8(b_df,r_df).show()
+# epic1.epic1_task9(b_df).show()
+# epic1.epic1_task10(b_df).show()
+# epic1.epic1_task11(b_df).show()
