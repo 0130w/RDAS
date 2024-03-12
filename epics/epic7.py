649f1196302c15625920681fb5310bbb316789ec
0130w <lovenvibest@gmail.com>
2024-03-13 00:17:08 +0800

    Update: add recommend and search framework

--- epics/epic7.py
+++ epics/epic7.py
@@ -2,7 +2,7 @@ from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.functions import udf, col
 from pyspark.sql.types import StringType
 from typing import Tuple
-from algorithms import geohash
+from algorithms import geohash, haversine
 
 
 def epic7_task1(location: Tuple[float, float], business_df: DataFrame, precision: int = 6) -> DataFrame:
@@ -11,4 +11,10 @@ def epic7_task1(location: Tuple[float, float], business_df: DataFrame, precision
     business_df = business_df.withColumn("geohash", geohash_udf(col("latitude"), col("longitude")))
     user_geohash = geohash.geohash_udf(latitude, longitude, precision)
     nearby_business_df = business_df.filter(business_df.geohash.startswith(user_geohash))
-    return nearby_business_df
+    sorted_nearby_business_df = haversine.haversine_distance(nearby_business_df, "latitude",
+                                                             "longitude", latitude, longitude)
+    return sorted_nearby_business_df
+
+
+def epic7_task2():
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import
 from pyspark.sql import SparkSession
+from epics import epic8
 
 # Driver
 spark = SparkSession \
@@ -14,3 +15,11 @@ checkin_path = 'dataset/yelp_academic_dataset_checkin.json'
 review_path = 'dataset/yelp_academic_dataset_review.json'
 tip_path = 'dataset/yelp_academic_dataset_tip.json'
 user_path = 'dataset/yelp_academic_dataset_user.json'
+
+review_df = spark.read.json(review_path)
+tip_df = spark.read.json(tip_path)
+business_df = spark.read.json(business_path)
+
+similarity = epic8.epic8_task1(spark, "_7bHUi9Uuf5__HHc_Q8guQ", tip_df, review_df, business_df)
+
+similarity.show(5)
